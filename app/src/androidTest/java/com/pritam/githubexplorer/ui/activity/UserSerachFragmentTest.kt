5522b932a3b4cbd1921586b4547d4f9f07fd7fc7
pritamkhose <pritamkhose@gmaio.com>
2019-07-16 22:20:01 +0530

    add testcase

---
+++ app/src/androidTest/java/com/pritam/githubexplorer/ui/activity/UserSerachFragmentTest.kt
@@ -0,0 +1,190 @@
+package com.pritam.githubexplorer.ui.activity
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.pritam.githubexplorer.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class UserSerachFragmentTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashScreenActivity::class.java)
+
+    @Test
+    fun userSerachFragmentTest2() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(3000)
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.textSearch),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("pritam"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.textSearch), withText("pritam"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(pressImeActionButton())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.textSearch), withText("pritam"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(click())
+
+        Thread.sleep(5000)
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.textSearch), withText("pritam"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(replaceText("Google"))
+
+        val appCompatEditText6 = onView(
+            allOf(
+                withId(R.id.textSearch), withText("Google"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText6.perform(closeSoftKeyboard())
+
+        val appCompatImageView = onView(
+            allOf(
+                withId(R.id.im_search),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageView.perform(click())
+
+
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(250)
+
+        val overflowMenuButton = onView(
+            allOf(
+                withContentDescription("More options"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.toolbar),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        overflowMenuButton.perform(click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(250)
+
+        val appCompatTextView = onView(
+            allOf(
+                withId(R.id.title), withText("Exit"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatTextView.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
--- app/src/test/java/com/pritam/githubexplorer/ExampleUnitTest.kt
+++ app/src/test/java/com/pritam/githubexplorer/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.pritam.githubexplorer
 
+import com.pritam.githubexplorer.ui.fragment.UsersDetailsFragment
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +11,47 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    var usersDetailsFragment = UsersDetailsFragment()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun stringtoDateFormat_isCorrect(){
+        assertEquals("14 Jul 2019", usersDetailsFragment.stringtoDateFormat("2019-07-14T06:56:42Z"))
+        assertEquals("23 Jan 2018", usersDetailsFragment.stringtoDateFormat("2018-01-23T16:14:34Z"))
+
+    }
+
+    @Test
+    fun stringtoDateFormat_isNotCorrect(){
+        assertNotEquals("2019/07/14T06:56:42Z", usersDetailsFragment.stringtoDateFormat("2019-07-14T06:56:42Z"))
+    }
+
+    @Test
+    fun isEmailValid_isCorrect(){
+        assertEquals(true , usersDetailsFragment.isEmailValid("p.khose04@gmail.com"))
+        assertEquals(true , usersDetailsFragment.isEmailValid("pritamkhose@gmail.co.in"))
+        assertEquals(true , usersDetailsFragment.isEmailValid("p112@gmail.com"))
+
+        assertEquals(true , usersDetailsFragment.isEmailValid("user@domain.com"))
+        assertEquals(true , usersDetailsFragment.isEmailValid("user@domain.co.in"))
+        assertEquals(true , usersDetailsFragment.isEmailValid("user.name@domain.com"))
+        assertEquals(true , usersDetailsFragment.isEmailValid("user?name@domain.co.in"))
+    }
+
+    @Test
+    fun isEmailValid_isNotCorrect(){
+        assertEquals(false , usersDetailsFragment.isEmailValid("gmail.com"))
+        assertEquals(false , usersDetailsFragment.isEmailValid("p.khosegmail.com"))
+
+        assertEquals(false , usersDetailsFragment.isEmailValid("@yahoo.com"))
+        assertEquals(false , usersDetailsFragment.isEmailValid(""))
+
+    }
+
+
 }
