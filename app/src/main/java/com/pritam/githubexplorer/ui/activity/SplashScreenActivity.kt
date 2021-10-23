db745103feaa0b17ac6c66d584cb7f3dc051d5e9
Pritam Khose <pritamkhose@gmail.com>
2021-10-23 23:16:07 +0530

    Dark theme support & Update dependencies

--- app/src/main/java/com/pritam/githubexplorer/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/pritam/githubexplorer/ui/activity/SplashScreenActivity.kt
@@ -1,41 +1,26 @@
 package com.pritam.githubexplorer.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.pritam.githubexplorer.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
-
-    private var mDelayHandler: Handler? = null
-    private val splashDelay: Long = 3000 //3 seconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        //Initialize the Handler
-        mDelayHandler = Handler()
-
-        //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, splashDelay)
-    }
-
-    private val mRunnable: Runnable = Runnable {
-        if (!isFinishing) {
+        //Initialize the Handler & Navigate with delay 3 Second
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }, 3000) //3 seconds
     }
-
-    override fun onDestroy() {
-        if (mDelayHandler != null) {
-            mDelayHandler!!.removeCallbacks(mRunnable)
-        }
-        super.onDestroy()
-    }
-
 
 }
